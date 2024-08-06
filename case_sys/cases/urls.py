332f28b59cde9572c27a6ed9a65544fa10450664
Ismail Mahfooz <ismailmahfooz113@gmail.com>
2024-08-06 14:46:39 +0500

    case_view page completed

--- case_sys/cases/urls.py
+++ case_sys/cases/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cases', views.case_view, name='case_view'),
 ]
--- case_sys/cases/views.py
+++ case_sys/cases/views.py
@@ -3,11 +3,15 @@ from .models import Case
 
 # Create your views here.
 def home(request):
-    query = request.GET.get('query', '')
-    
-    if query:
-        cases = Case.objects.filter(case_id__icontains=query)
-    else:
-        cases = Case.objects.all()  # Return all cases if no query is provided
-    
-    return render(request, 'home.html', {'cases': cases, 'query': query})
+    cases = Case.objects.all()
+    return render(request, 'home.html', {'cases': cases})
+
+#search function for cases
+def search(request):
+    query = request.GET['query']
+    cases = Case.objects.filter(title__icontains=query)
+    return render(request, 'home.html', {'cases': cases})
+
+#view case function
+def case_view(request):
+    return render(request, 'case_view.html')
