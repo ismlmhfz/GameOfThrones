3417cd278d626c563e2f80d4125283f570a6cbda
ismlmahfooz <100115565+ismlmahfooz@users.noreply.github.com>
2024-08-06 21:09:48 +0500

    search update - pagination

--- case_sys/case_sys/settings.py
+++ case_sys/case_sys/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cases',
+    'ckeditor',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -132,3 +134,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
--- case_sys/cases/views.py
+++ case_sys/cases/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from .models import Case
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
-    cases = Case.objects.all()
-    return render(request, 'home.html', {'cases': cases})
+    query = request.GET.get('query', '')
+    
+    if query:
+        cases_list = Case.objects.filter(
+            Q(case_name__icontains=query) |
+            Q(case_description__icontains=query) |
+            Q(case_status__icontains=query) |
+            Q(case_priority__icontains=query) |
+            Q(case_assignee__icontains=query)
+        )
+    else:
+        cases_list = Case.objects.all()  # Retrieve all cases if no query is provided
 
-#search function for cases
-def search(request):
-    query = request.GET['query']
-    cases = Case.objects.filter(title__icontains=query)
-    return render(request, 'home.html', {'cases': cases})
+    paginator = Paginator(cases_list, 10)  # Show 10 cases per page
+    page_number = request.GET.get('page')  # Get current page number from query params
+    cases = paginator.get_page(page_number)  # Get cases for the current page
+
+    return render(request, 'home.html', {'cases': cases, 'query': query})
 
 #view case function
 def case_view(request):
