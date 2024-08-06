59e83a6111fdb2428418e9cd4ca4baced63177d4
ismlmahfooz <100115565+ismlmahfooz@users.noreply.github.com>
2024-08-06 07:54:40 +0500

    new logo

--- case_sys/case_sys/settings.py
+++ case_sys/case_sys/settings.py
@@ -116,8 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# Directory where collectstatic will collect static files for deployment
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- case_sys/cases/views.py
+++ case_sys/cases/views.py
@@ -3,6 +3,11 @@ from .models import Case
 
 # Create your views here.
 def home(request):
-    cases = Case.objects.all()
-    return render(request, 'home.html', {'cases': cases})
-
+    query = request.GET.get('query', '')
+    
+    if query:
+        cases = Case.objects.filter(case_id__icontains=query)
+    else:
+        cases = Case.objects.all()  # Return all cases if no query is provided
+    
+    return render(request, 'home.html', {'cases': cases, 'query': query})
